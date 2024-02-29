8f8bab51c4d039e5699729a13cc74c0e52ca0f19
Lovette Duke <qutyluv@gmail.com>
2024-02-29 20:51:05 +0100

    Task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with child
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: 0 or node count
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t node_c = 0;
+
+	if (tree)
+	{
+		node_c += (tree->left || tree->right) ? 1 : 0;
+		node_c += binary_tree_nodes(tree->left);
+		node_c += binary_tree_nodes(tree->right);
+	}
+	return (node_c);
+}
